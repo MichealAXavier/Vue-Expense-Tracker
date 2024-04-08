94f69345100ffe4b71e887b7d3eafaefec737bd5
MICHEAL XAVIER A <armxavier1@gmail.com>
2024-04-08 17:58:20 +0530

    Initial commit

---
+++ src/components/Balance.vue
@@ -0,0 +1,17 @@
+<template>
+    <h4>Your Balance</h4>
+    <h1 id="balance">Rs{{ total }}</h1>
+</template>
+
+
+<script setup>
+//setup() function is used inside a Vue component to initialize the component's state, 
+//define reactive properties, set up event listeners, 
+//and perform other initialization tasks.
+const props = defineProps({
+    total: {
+        type: Number,
+        required: true,
+    },
+});
+</script>
